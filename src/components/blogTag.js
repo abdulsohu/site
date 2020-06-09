a4771f91c9274407789b503b1d132be43e43980c
abdulmoizmunir <sohu@sas.upenn.edu>
2020-06-09 16:47:08 -0500

    add support for unlisted tags

--- src/components/blogTag.js
+++ src/components/blogTag.js
@@ -1,8 +1,9 @@
 import React from "react"
 require("../styles/styles.scss")
+var Color = require("color")
 
 function getColour(name, size) {
-  let sizer = ""
+  let sizer = " is-normal"
   if (size) {
     sizer = " " + size
   }
@@ -11,19 +12,55 @@ function getColour(name, size) {
   if (name === "personal") return "is-danger is-light" + sizer
   if (name === "design") return "is-primary is-light" + sizer
   if (name === "notes") return "is-warning is-light" + sizer
-  if (name === "programming") return "is-info is-light" + sizer
-  if (name === "random") return "is-link is-light" + sizer
+  if (name === "series") return "is-info is-light" + sizer
+  if (name === "programming") return "is-link is-light" + sizer
 
   return sizer
 }
 
+function hashCode(str) {
+  var hash = 0
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash)
+  }
+  return hash
+}
+
+function intToRGB(i) {
+  var c = (i & 0x00ffffff).toString(16).toUpperCase()
+  return "#" + "00000".substring(0, 6 - c.length) + c
+}
+
+function createColour(name) {
+  return intToRGB(hashCode(name))
+}
+
 const blogTag = props => {
+  var attributes = getColour(props.name, props.size)
+
+  if (attributes === " is-normal") {
+    const color = Color(createColour(props.name)).alpha(0.5).lighten(0.1)
+    const inverted = Color(createColour(props.name)).darken(0.5)
+
+    return (
+      <a href={"/tags/" + props.name}>
+        <span
+          className={"tag" + attributes}
+          style={{
+            backgroundColor: color,
+            color: inverted,
+          }}
+        >
+          {props.name}
+        </span>{" "}
+        &nbsp;
+      </a>
+    )
+  }
+
   return (
     <a href={"/tags/" + props.name}>
-      <span className={"tag " + getColour(props.name, props.size)}>
-        {props.name}
-      </span>{" "}
-      &nbsp;
+      <span className={"tag " + attributes}>{props.name}</span> &nbsp;
     </a>
   )
 }
